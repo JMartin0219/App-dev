6f379581ed4cebf665017fe7c875cedc67d8f676
JMartin0219 <jegarnica@paterostechnologicalcollege.edu.ph>
2025-02-19 00:04:43 +0800

    added course to the code

--- Prog1.java
+++ Prog1.java
@@ -1,6 +1,9 @@
 public class Prog1 {
     public static void main(String[] args) {
-        System.out.printIn("PATEROS TECHNOLOGICAL COLLEGE");
+        System.out.printIn("PATEROS");
+        System.out.printIn("TECHNOLOGICAL");
+        System.out.printIn("COLLEGE");
+        System.out.printIn("COURSE: BSIT");
 
     }
 }
